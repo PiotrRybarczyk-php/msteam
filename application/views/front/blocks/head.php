fc4829cc09d242b9fcefc2c8b7672711c5d20301
PiotrRybarczyk-php <piotr.rybarczyk1994@gmail.com>
2021-07-08 15:26:15 +0200

    front layout

--- application/views/front/blocks/head.php
+++ application/views/front/blocks/head.php
@@ -6,5 +6,6 @@
   <meta name="viewport" content="width=device-width, initial-scale=1" />
   <link href="<?= base_url(); ?>assets/front/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous" />
   <link href="<?= base_url(); ?>assets/front/css/style.css" rel="stylesheet" crossorigin="anonymous" />
+  <link href="<?= base_url(); ?>assets/front/css/qgrid.css" rel="stylesheet" crossorigin="anonymous" />
   <title>Hello, world!</title>
 </head>
--- application/views/front/pages/index.php
+++ application/views/front/pages/index.php
@@ -52,18 +52,77 @@
                 <div class="content-section">
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam tempus odio ut arcu euismod consectetur. Etiam fermentum enim sit amet dolor consequat efficitur. Pellentesque feugiat odio quis placerat rutrum. Aenean et tincidunt massa. Duis accumsan, odio eu vestibulum pharetra, nisi augue pulvinar ipsum, congue hendrerit nisi turpis vehicula diam. Phasellus posuere eleifend diam in malesuada. Donec euismod nunc sed dignissim bibendum. Integer id justo in erat pharetra venenatis viverra ac elit. Praesent sodales augue sed placerat commodo. Sed nec dictum ante. Nulla in lobortis risus lectus.
                 </div>
-                <div class="row">
-                    <div class="col-12 col-md-6 col-lg-4 mx-auto">
+            </div>
+            <div class="grid-1 main_padding">
+                <div class="grid-3 gtab-1 mb-5">
+                    <div class="item_container_center small_padding">
+                        <div class="absolute_el" style="top:0px;left:0px;"><span class="line_a_h_black"></span><span class="line_a_h_cyan"></span><span class="line_a_v_black"></span></div>
                         <div class="offer-photo-border">
-                            <div class="offer-photo" style="background-image: url(<?= base_url(); ?>assets/front/img/electrical-engineer-using-digital-multi-meter-checking-electric-current-voltage-circuit-breaker.jpg);"></div>
+                            <div class="offer-photo" style="background-image: url(<?= base_url(); ?>assets/front/img/electrical-engineer-using-digital-multi-meter-checking-electric-current-voltage-circuit-breaker.jpg);">
+                                <div class="img_overlay">
+                                    <p>Sieci i Instalacje Elektryczne</p>
+                                </div>
+                            </div>
                         </div>
                     </div>
-                    <div class="col-12 col-md-6 col-lg-4 mx-auto">
-                        <div class="offer-photo" style="background-image: url(<?= base_url(); ?>assets/front/img/electrical-engineer-using-digital-multi-meter-checking-electric-current-voltage-circuit-breaker.jpg);"></div>
+                    <div class="item_container_center small_padding">
+                        <div class="offer-photo-border">
+                            <div class="offer-photo" style="background-image: url(<?= base_url(); ?>assets/front/img/solar-panel-with-blue-sky-sunset-concept-clean-energy-electric-alternative-power-nature.jpg);">
+                                <div class="img_overlay">
+                                    <p>Fotowoltaika</p>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                    <div class="col-12 col-md-6 col-lg-4 mx-auto">
-                        <div class="offer-photo" style="background-image: url(<?= base_url(); ?>assets/front/img/electrical-engineer-using-digital-multi-meter-checking-electric-current-voltage-circuit-breaker.jpg);"></div>
+                    <div class="item_container_center small_padding">
+                        <div class="absolute_el" style="bottom:0px;right:0px;"><span class="line_b_h_black"></span><span class="line_b_v_cyan"></span><span class="line_b_v_black"></span></div>
+                        <div class="offer-photo-border">
+                            <div class="offer-photo" style="background-image: url(<?= base_url(); ?>assets/front/img/excavator-sandpit-during-earthmoving-works-construction-site.jpg);">
+                                <div class="img_overlay">
+                                    <p>Roboty Ziemne</p>
+                                </div>
+                            </div>
+                        </div>
                     </div>
+                </div>
+                <div class="grid-3 gtab-1 mt-5">
+                    <div></div>
+                    <div></div>
+                    <div class="item_container_center">
+                        <a class="blank" href="#"><button class="btn_JS">Sprawdź Ofertę</button></a>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section>
+            <div class="container">
+                <div class="header-section">
+                    <div class="separator-line"></div>
+                    <div class="header-title">JESTEŚMY RENOMOWANYM WYKONAWCĄ INSTALACJI ELEKTRYCZNYCH</div>
+                    <div class="separator-line"></div>
+                </div>
+            </div>
+            <div class="grid-2 gtab-1 main_padding">
+                <div class="item_container">
+                    <div class="inner_container_1 small_padding">
+                        <div class="absolute_el tab_hide" style="bottom:0px;right:0px;"><span class="line_b_h_black"></span><span class="line_b_v_cyan"></span><span class="line_b_v_black"></span></div>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus efficitur scelerisque posuere. Pellentesque ultrices semper velit nec cursus. Interdum et malesuada fames ac ante ipsum primis in faucibus. Sed et porttitor nibh, eget porta sapien. Pellentesque elementum dolor risus, varius pellentesque odio volutpat id. Ut nec elit et enim imperdiet tempus id nec ipsum. Suspendisse velit ex, lacinia sit amet feugiat ac, gravida eget dui. Nunc faucibus condimentum magna sed luctus. Nullam luctus quam metus, eu accumsan lectus fermentum et. Nam ac varius enim, ut porttitor leo. Curabitur imperdiet diam non mattis feugiat.</p>
+                        <p>Vivamus nec nisl eros. Ut pellentesque nulla feugiat metus rutrum, vitae vestibulum felis faucibus. Morbi nec magna non nisl ornare lobortis. Nunc tortor sapien, dignissim at congue ac, feugiat id nunc. Aenean at augue et diam ultrices congue. Donec viverra erat malesuada massa sollicitudin mollis. Donec interdum ullamcorper tellus, ac pulvinar diam varius varius nulla.V</p>
+                        <div class="item_flex flex_r tab_hide"><a class="blank" href="#"><button class="btn_JS">Poznaj Nas</button></a></div>
+                    </div>
+                </div>
+                <div class="item_container_align small_padding">
+                    <div class="offer-photo" style="background-image: url(<?= base_url(); ?>assets/front/img/excavator-sandpit-during-earthmoving-works-construction-site.jpg);"></div>
+                </div>
+                <div class="item_container_center tab_show mt-5"><a class="blank" href="#"><button class="btn_JS">Poznaj Nas</button></a></div>
+            </div>
+        </section>
+        <section>
+            <div class="container">
+                <div class="header-section">
+                    <div class="separator-line"></div>
+                    <div class="header-title">FACHOWI I KOMPETENTNI</div>
+                    <div class="separator-line"></div>
                 </div>
             </div>
         </section>
