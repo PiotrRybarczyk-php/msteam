bd437990e66afb0889dbc3550e911aa877d0e831
PiotrRybarczyk-php <piotr.rybarczyk1994@gmail.com>
2021-07-13 12:24:51 +0200

    layout

--- application/views/front/pages/blog.php
+++ application/views/front/pages/blog.php
@@ -29,7 +29,7 @@
         </div>
     </div>
     <hr class="tab_show" style="margin-left:1.2vw;margin-right:1.2vw;">
-    <div class="box_flex flex_mob main_padding">
+    <div class="box_flex flex_mob main_padding tab_hide">
         <div class="grid-2 gtab-1 mb-5" style="min-height:504px;max-width:1440px;">
             <div class="box_flex flex_c small_padding">
                 <div class="grid-1" style="max-width:540px;">
@@ -37,7 +37,7 @@
                         <p class="second_header">Tytuł Artykułu</p>
                     </div>
                     <p class="content_with_padding">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus in sodales tellus, quis egestas lorem. Quisque sit amet risus suscipit, aliquam enim ac, rhoncus diam. In faucibus nibh sed sem efficitur, in dapibus sapien venenatis. Sed odio ante, maximus nec nisi ac, dapibus aliquam nibh. Duis consequat luctus felis vitae hendrerit. Nam mattis ultricies fermentum. Nullam ullamcorper elit proin.</p>
-                    <div class="box_flex flex_mob tab_hide"><a class="blank" href="<?= base_url('wpis/2/test'); ?>"><button class="btn_static">czytaj więcej</button></a></div>
+                    <div class="box_flex flex_mob"><a class="blank" href="<?= base_url('wpis/2/test'); ?>"><button class="btn_static">czytaj więcej</button></a></div>
                 </div>
             </div>
             <div class="box_flex flex_c" style="min-height:500px;">
@@ -45,8 +45,43 @@
                     <div class="item_container_picture" style="max-width:642px;background-image:url(<?= base_url(); ?>assets/front/img/ms_photo_3.jpg)"></div>
                 </div>
             </div>
-            <div class="box_flex flex_mob tab_show mt-1"><a class="blank" href="<?= base_url('wpis/2/test'); ?>"><button class="btn_static">czytaj więcej</button></a></div>
+        </div>
+    </div>
+    <div class="box_flex flex_mob_l main_padding tab_show">
+        <div class="grid-2 gtab-1 mb-5" style="min-height:504px;max-width:1440px;">
+            <div class="box_flex flex_c" style="min-height:500px;">
+                <div class="work-photo-border">
+                    <div class="item_container_picture" style="max-width:642px;background-image:url(<?= base_url(); ?>assets/front/img/ms_photo_3.jpg)"></div>
+                </div>
+            </div>
+            <div class="box_flex flex_c small_padding">
+                <div class="grid-1" style="max-width:540px;">
+                    <div class="box_flex">
+                        <p class="second_header">Tytuł Artykułu</p>
+                    </div>
+                    <p class="content_with_padding">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus in sodales tellus, quis egestas lorem. Quisque sit amet risus suscipit, aliquam enim ac, rhoncus diam. In faucibus nibh sed sem efficitur, in dapibus sapien venenatis. Sed odio ante, maximus nec nisi ac, dapibus aliquam nibh. Duis consequat luctus felis vitae hendrerit. Nam mattis ultricies fermentum. Nullam ullamcorper elit proin.</p>
+                    <div class="box_flex flex_mob_l"><a class="blank" href="<?= base_url('wpis/2/test'); ?>"><button class="btn_static">czytaj więcej</button></a></div>
+                </div>
+            </div>
         </div>
     </div>
     <hr class="tab_show" style="margin-left:1.2vw;margin-right:1.2vw;">
+    <div class="box_flex flex_mob_l main_padding">
+        <div class="grid-2 gtab-1 mb-5" style="min-height:504px;max-width:1440px;">
+            <div class="box_flex flex_c" style="min-height:500px;">
+                <div class="work-photo-border">
+                    <div class="item_container_picture" style="max-width:642px;background-image:url(<?= base_url(); ?>assets/front/img/ms_photo_6.jpg)"></div>
+                </div>
+            </div>
+            <div class="box_flex flex_c small_padding">
+                <div class="grid-1" style="max-width:540px;">
+                    <div class="box_flex">
+                        <p class="second_header">Tytuł Artykułu</p>
+                    </div>
+                    <p class="content_with_padding">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus in sodales tellus, quis egestas lorem. Quisque sit amet risus suscipit, aliquam enim ac, rhoncus diam. In faucibus nibh sed sem efficitur, in dapibus sapien venenatis. Sed odio ante, maximus nec nisi ac, dapibus aliquam nibh. Duis consequat luctus felis vitae hendrerit. Nam mattis ultricies fermentum. Nullam ullamcorper elit proin.</p>
+                    <div class="box_flex flex_mob_l"><a class="blank" href="<?= base_url('wpis/3/test'); ?>"><button class="btn_static">czytaj więcej</button></a></div>
+                </div>
+            </div>
+        </div>
+    </div>
 </section>
--- application/views/front/pages/work.php
+++ application/views/front/pages/work.php
@@ -30,7 +30,7 @@
         </div>
     </div>
     <hr class="tab_show" style="margin-left:1.2vw;margin-right:1.2vw;">
-    <div class="box_flex flex_mob main_padding">
+    <div class="box_flex flex_mob main_padding tab_hide">
         <div class="grid-2 gtab-1 mb-5" style="min-height:504px;max-width:1440px;">
             <div class="box_flex flex_c small_padding">
                 <div class="grid-1" style="max-width:540px;">
@@ -38,7 +38,7 @@
                         <p class="second_header">Stanowisko</p>
                     </div>
                     <p class="content_with_padding">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus in sodales tellus, quis egestas lorem. Quisque sit amet risus suscipit, aliquam enim ac, rhoncus diam. In faucibus nibh sed sem efficitur, in dapibus sapien venenatis. Sed odio ante, maximus nec nisi ac, dapibus aliquam nibh. Duis consequat luctus felis vitae hendrerit. Nam mattis ultricies fermentum. Nullam ullamcorper elit proin.</p>
-                    <div class="box_flex flex_mob_l tab_hide"><a class="blank" href="#"><button class="btn_static">kontakt</button></a></div>
+                    <div class="box_flex flex_mob_l"><a class="blank" href="#"><button class="btn_static">kontakt</button></a></div>
                 </div>
             </div>
             <div class="box_flex flex_c" style="min-height:500px;">
@@ -46,8 +46,43 @@
                     <div class="item_container_picture" style="max-width:642px;background-image:url(<?= base_url(); ?>assets/front/img/ms_photo_9.jpg)"></div>
                 </div>
             </div>
-            <div class="box_flex flex_mob tab_show mt-1"><a class="blank" href="#"><button class="btn_static">kontakt</button></a></div>
+        </div>
+    </div>
+    <div class="box_flex flex_mob_l main_padding tab_show">
+        <div class="grid-2 gtab-1 mb-5" style="min-height:504px;max-width:1440px;">
+            <div class="box_flex flex_c" style="min-height:500px;">
+                <div class="work-photo-border">
+                    <div class="item_container_picture" style="max-width:642px;background-image:url(<?= base_url(); ?>assets/front/img/ms_photo_9.jpg)"></div>
+                </div>
+            </div>
+            <div class="box_flex flex_c small_padding">
+                <div class="grid-1" style="max-width:540px;">
+                    <div class="box_flex">
+                        <p class="second_header">Stanowisko</p>
+                    </div>
+                    <p class="content_with_padding">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus in sodales tellus, quis egestas lorem. Quisque sit amet risus suscipit, aliquam enim ac, rhoncus diam. In faucibus nibh sed sem efficitur, in dapibus sapien venenatis. Sed odio ante, maximus nec nisi ac, dapibus aliquam nibh. Duis consequat luctus felis vitae hendrerit. Nam mattis ultricies fermentum. Nullam ullamcorper elit proin.</p>
+                    <div class="box_flex flex_mob_l"><a class="blank" href="#"><button class="btn_static">kontakt</button></a></div>
+                </div>
+            </div>
         </div>
     </div>
     <hr class="tab_show" style="margin-left:1.2vw;margin-right:1.2vw;">
+    <div class="box_flex flex_mob_l main_padding">
+        <div class="grid-2 gtab-1 mb-5" style="min-height:504px;max-width:1440px;">
+            <div class="box_flex flex_c" style="min-height:500px;">
+                <div class="work-photo-border">
+                    <div class="item_container_picture" style="max-width:642px;background-image:url(<?= base_url(); ?>assets/front/img/ms_photo_9.jpg)"></div>
+                </div>
+            </div>
+            <div class="box_flex flex_c small_padding">
+                <div class="grid-1" style="max-width:540px;">
+                    <div class="box_flex">
+                        <p class="second_header">Stanowisko</p>
+                    </div>
+                    <p class="content_with_padding">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus in sodales tellus, quis egestas lorem. Quisque sit amet risus suscipit, aliquam enim ac, rhoncus diam. In faucibus nibh sed sem efficitur, in dapibus sapien venenatis. Sed odio ante, maximus nec nisi ac, dapibus aliquam nibh. Duis consequat luctus felis vitae hendrerit. Nam mattis ultricies fermentum. Nullam ullamcorper elit proin.</p>
+                    <div class="box_flex flex_mob_l"><a class="blank" href="#"><button class="btn_static">kontakt</button></a></div>
+                </div>
+            </div>
+        </div>
+    </div>
 </section>
