f14af3fb0e7c7be833d8e9481b7eec027ad48adc
PiotrRybarczyk-php <piotr.rybarczyk1994@gmail.com>
2021-07-15 11:40:13 +0200

    backpanel complete

--- application/controllers/panel/Main_Projects.php
+++ application/controllers/panel/Main_Projects.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 
-class Main_Projects extends CI_Controller
+class Main_projects extends CI_Controller
 {
 
 	public function index()
